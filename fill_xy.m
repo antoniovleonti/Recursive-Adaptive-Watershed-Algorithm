b941d30f5a646a1e4cefe6f917c9d3665c9822a5
Antonio V. Leonti <53806445+antoniovleonti@users.noreply.github.com>
2020-04-09 14:40:02 -0400

    experimenting with morphological transformations

--- fill_xy.m
+++ fill_xy.m
@@ -4,7 +4,8 @@ function filled = fill_xy(data)
     shape = size(data);
     
     for z = 1 : shape(3)
-       data(1:end, 1:end, z) = imfill(data(1:end, 1:end, z),'holes');
+        
+       data(1:end, 1:end, z) = imfill(data(1:end, 1:end, z), 'holes');
     end
     
     filled = data;
